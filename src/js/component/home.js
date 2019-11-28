7ef77ac32a474e60198f41d512b42cef4cbab380
AeLo-19 <adrianeduardo.lopezo@gmail.com>
2019-11-28 23:40:19 +0000

    funciones de la API

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,22 +4,89 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+var apiUrl ="https://assets.breatheco.de/apis/fake/todos/user/AeLo-19"
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			tasks: [
-				"Hacer café",
-				"Hacer la cama",
-				"Tomar agua",
-				"Regar las plantas"
-			],
-			newTask: ""
+		 this.state = {
+			tasks: [],
+		 	newTask: ""
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleAddTask = this.handleAddTask.bind(this);
 		this.handleDelete = this.handleDelete.bind(this);
-	}
+    }
+    
+    componentDidMount() {
+        //solicita tareas
+        fetch(apiUrl,
+            {
+                method: "GET",
+                header: {
+                    "Content-Type": "application/JSON"
+                }
+                
+            }
+        ) .then(response => {
+            let res = response.clone();
+            console.log(res.status);
+
+            if (res,status == 404) {
+                let emptyArray = [];
+                fetch(
+                    apiUrl,
+                    {
+                        method: "POST",
+                        body: JSON.stringify(emptyArray),
+                        headers: {
+                            "Content-type": "application/JSON"
+                        }
+                    }
+
+                ) .then(secondResponse => {
+                    console.log("tried to create user: ");
+                    console.log(secondResponse.status);
+                    console.log(secondResponse.text());
+                    if (secondResponse.ok) {
+                        fetch(
+                            apiUrl,
+                            {
+                                method: "GET",
+                                headers:{
+                                    "Content-Type": "application/JSON"
+                                }
+                            }
+                        ) .then(thirdResponse => {
+                            return thirdResponse.json();
+                        })
+                        .catch(error => console.log(error));            
+                } else {
+                    return [
+                        {
+                            label: "ERROR, user not created",
+                            donde: false
+                        }
+                    ];
+                }
+            }) 
+            .catch(error => {
+                console.log(error);
+            });
+        }  else {
+            return response.json();
+        }
+    }) .then(data => {
+        console.log(data);
+        this.setState({
+            task: data,
+
+            newTask: this.state.newTask
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
 	handleDelete(event, id) {
 		console.log(event, id);
 		let tasksLeft = this.state.tasks.filter((value, index) => {
@@ -32,18 +99,126 @@ class Home extends React.Component {
 		});
 	}
 	handleAddTask(event) {
-		event.preventDefault();
-		this.setState({
-			tasks: [...this.state.tasks, this.state.newTask],
-			newTask: ""
-		});
-	}
+        event.preventDefault();
+        let currentTasks = this.state.tasks;
+        currentTasks.push(
+            {
+                label: this.state.newTask,
+                done: false
+            });
+            console.log(currentTasks);
+            fetch(
+                apiUrl,
+                {
+                    method: "PUT",
+                    body: JSON.stringify(currentTasks),
+                    headers: {
+                        "Content-Type": "application/JSON"
+                    }
+                }
+            ) .then( response => {
+                if (response.ok) {
+                    fetch(
+                        apiUrl,
+                        {
+                            method: "GET",
+                            headers: {
+                                "Content-Type": "application/JSON"
+                            }
+                        }
+                    ) .then(secondResponse => {
+                        return secondResponse.json();
+                    }) .then (dat => {
+                        this.setState({
+                            tasks: data,
+                            newTask: ""
+                        });
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+                } else {
+                    console.log("error fetching tasks ", response.text());
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+	
 	handleChange(event) {
 		this.setState({
-			tasks: this.state.tasks,
+			...this.state,
 			newTask: event.target.value
 		});
-	}
+    }
+    handleDeleteAll(event){
+        fetch(
+            apiUrl,
+            {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/JSON"
+                }
+            }
+        ) .then(response => {
+            if (response.ok) {
+                console.log(
+                    "taks deleted! ",
+                    response.status,
+                    response.text()
+                );
+                let emptyArray = []
+                fetch(
+                    apiUrl,
+                    {
+                        method: "POST",
+                        body: JSON.stringify(emptyArray),
+                        headers: {
+                            "Content-Type": "application/JSON"
+                        }
+                    }
+                ) .then(secondResponse => {
+                    console.log("tried to create user: ");
+                    console.log(secondResponse.status);
+                    console.log(secondResponse.text());
+                    if (secondResponse.ok) {
+                        fetch(
+                            apiUrl,
+                            {
+                                    method: "GET",
+                                    headers: {
+                                    "Content-Type": "application/JSON"
+                                    }
+                            }
+                        ) .then(thirdResponse => {
+                            return thirdResponse.json();
+                        }) .then(data => {
+                            this.setState({
+                                tasks: data,
+                                newTask: ""
+                            });
+                        })
+                        .catch(error => console.log(error));
+                    } else {
+                        this.setState({
+                            label: "ERROR, user not created",
+							done: false
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            } else {
+                console.log(
+                "couldn't delete tasks because: ",
+                response.text()
+            );
+        }
+    })
+    .catch(error => console.log(error));
+}
 	render() {
 		let tasks = this.state.tasks;
 		return (
